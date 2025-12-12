8cf50c6e5b8ea26c9deac206e0a04d3b46d6d45a
krit <krittapasboonruang@gmail.com>
2025-12-12 12:51:28 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,23 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    int n, even=0, odd=0;
-while (true)
-{
-    cout << "Enter an integer: ";
-    cin >> n;
-    if (n ==0){
-        break;
-    }
-}
-if (n%2==0){
-    even++;
-} else {
-    odd++;
-}
+int main() {
+    int n;
+    int even = 0;
+    int odd = 0; 
     
-    cout << "\n#Even numbers = " << even << "\n";
-    cout << "\n#Odd numbers = " << odd << "\n";
+    while (true) {
+        cout << "Enter an integer: ";
+        cin >> n;
+        if (n == 0) {
+            break;
+        }
+        if (n % 2 == 0) {
+            even++; 
+        } else {
+            odd++;
+        }
+    }
+    cout << "#Even numbers = " << even;
+    cout << "\n#Odd numbers = " << odd;
     return 0;
 }
